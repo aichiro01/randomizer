e959c5198f5b9776141da76dc1a51b33e8688574
aichiro01 <90949198+aichiro01@users.noreply.github.com>
2022-03-11 21:29:13 -0800

    changes

--- sketch.js
+++ sketch.js
@@ -1,19 +1,43 @@
 //Japanse Culture
 
-let facts = ["armor", "katana", "sake", "mask", "robe"];
+let facts = [{
+      name: "armor",
+      color: "black color"
+    }, {
+      name: "katana",
+      color: "gold"
+    }, {
+      name: "sake",
+      color: "white color"
+    }, {
+      name: "mask",
+      color: "dark oak"
+    }, {
+      name: "robe",
+      color: "baige color"}];
+
+let randomIndex;
+
+    function setup() {
+      createCanvas(600, 600);
+      background(200);
 
 
 
-function setup() {
-  createCanvas(600, 600);
-
-console.log(facts[2]);
 
 
 
 
-}
 
-function draw() {
-  background(200);
-}
+    }
+
+    function draw() {
+
+    }
+
+    function mousePressed(){
+      background(random(200, 255));
+      randomIndex = int(random(facts.length));
+      text(facts[randomIndex].name, 50, 50);
+      facts.splice(randomIndex, 1);
+    }
