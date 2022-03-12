dcdb4f195605f6f851639ace66d4d2f0a8129c84
aichiro01 <90949198+aichiro01@users.noreply.github.com>
2022-03-11 20:26:24 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,17 @@
 //Japanse Culture
 
-let facts = ["armor", "katana", "sake", "mask", "robe"]
+let facts = ["armor", "katana", "sake", "mask", "robe"];
 
 
 
 function setup() {
   createCanvas(600, 600);
+
+console.log(facts[0]);
+
+
+
+
 }
 
 function draw() {
